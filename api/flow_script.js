2d29a6973ef2478dd44e5fc3948b531cced68d78
wikro <robert@wikro.org>
2024-02-21 15:51:54 +0100

    For safe keeping

---
+++ api/flow_script.js
@@ -0,0 +1,21 @@
+module.exports = async function(data) {
+    let jobs = data.targets.map(target => {
+
+        let labels = target.labels.reduce((total, label) => {
+            total[label.name.trim()] = label.value.trim();
+            return total;
+        }, {});
+
+        let meta_labels = {
+            "__meta_directus_job": target.job ? target.job.name.trim() : "directus"
+        };
+
+        return {
+            "targets": [target.address.trim()],
+            "labels": { ...meta_labels, ...labels }
+        };
+
+    });
+
+    return jobs;
+};
