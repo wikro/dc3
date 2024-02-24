bd25bcd2fc895352bf870bd0a81a291dc6ee39f1
wikro <robert@wikro.org>
2024-02-24 15:50:59 +0100

    Testing out pocketbase sdk

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -1,6 +1,33 @@
-const pb = new PocketBase("https://pocketbase.dc3.se")
+const { createApp, ref } = Vue;
 
-pb.collection('chat').subscribe('*', function (e) {
-    console.log(e.action);
-    console.log(e.record);
-}, {});
+const setup = () => {
+  const pb = new PocketBase('https://pocketbase.dc3.se');
+
+  const messages = ref([]);
+
+  const fetchMessages = () => {
+    pb.collection('chat').getList(1, 15, {}).then((list) => {
+      messages.value = list.items;
+    });
+  };
+
+  fetchMessages();
+
+  const newMessage = (message) => {
+    messages.value.push(message);
+
+    if (messages.value.length > 15) {
+      messages.value = messages.value.slice(1, 17);
+    };
+  };
+
+  pb.collection('chat').subscribe('*', (e) => {
+    newMessage(e.record);
+  }, {});
+
+  return { messages };
+};
+
+const app = createApp({ setup });
+
+app.mount('#chat');
