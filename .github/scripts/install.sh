f0bde0f781d1ec41a64baebb275cd0213e778a27
wikro <robert@wikro.org>
2024-02-25 12:20:31 +0100

    Fixes

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
+cd /tmp
 md5sum -c md5sums.txt || exit 1
 
 # Temp deploy dir
-mkdir -p /tmp/deploy
+mkdir -p deploy
 
 # Install web
-if [ -f /tmp/web.tar ]; then
-  tar -xf /tmp/web.tar -C /tmp/deploy
+if [ -f web.tar ]; then
+  tar -xf web.tar -C deploy/
 
   rm -rf /var/www/html/*
   rm -rf /var/www/html/.*
-  cp -R /tmp/deploy/web/* /var/www/html/
+  cp -R deploy/web/* /var/www/html/
 
   chmod -R ug+rwX,o+rX /var/www/html
 fi
 
 # Install api
-if [ -f /tmp/api.tar ]; then
-  tar -xf /tmp/api.tar -C /tmp/deploy
+if [ -f api.tar ]; then
+  tar -xf api.tar -C deploy/
 
   rm -rf /opt/directus/*
   rm -rf /opt/directus/.*
-  cp -R /tmp/deploy/api/* /opt/directus/
+  cp -R deploy/api/* /opt/directus/
 
   chmod -R ug+rwX,o+rX /opt/directus
 
@@ -31,17 +32,17 @@ if [ -f /tmp/api.tar ]; then
 fi
 
 # Install pocketbase
-if [ -f /tmp/pb.tar ]; then
-  tar -xf /tmp/pb.tar -C /tmp/deploy
+if [ -f pb.tar ]; then
+  tar -xf pb.tar -C deploy/
 
   rm -rf /opt/pocketbase/*
   rm -rf /opt/pocketbase/.*
 
-  cp -R /tmp/deploy/pocketbase/* /opt/pocketbase/
+  cp -R deploy/pocketbase/* /opt/pocketbase/
 
   chmod -R ug+rwX,o+rX /opt/directus
   sudo systemctl restart pocketbase
 fi
 
 # Clean up
-rm -rf /tmp/web.tar /tmp/api.tar /tmp/pb.tar /tmp/deploy/ /tmp/install.sh
+rm -rf web.tar api.tar pb.tar deploy/ install.sh
