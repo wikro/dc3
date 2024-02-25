083f81bf426f5e990b21da1f692ff0ebd5407a28
wikro <robert@wikro.org>
2024-02-25 15:26:36 +0100

    Oops

--- .github/scripts/api_deploy.sh
+++ .github/scripts/api_deploy.sh
@@ -2,7 +2,7 @@
 
 mv api/*.tmpl .
 
-`dirname "$0"`/tools/render_template.sh directus.env.tmpl > api/directus.env
-`dirname "$0"`/tools/render_template.sh postgresql.env.tmpl > api/postgresql.env
+source `dirname "$0"`/tools/render_template.sh directus.env.tmpl > api/directus.env
+source `dirname "$0"`/tools/render_template.sh postgresql.env.tmpl > api/postgresql.env
 
 source `dirname "$0"`/tools/deploy.sh api
