374aa1718b5f4c8995c9b8eec9b592fceae2c401
wikro <robert@wikro.org>
2024-02-27 00:43:22 +0100

    Test

--- pb/hooks/main.pb.js
+++ pb/hooks/main.pb.js
@@ -1,4 +1,4 @@
-routerAdd("GET", "/gh", (c) => {
+routerAdd("GET", "/gh/", (c) => {
   const latest = new Record();
 
   $app.dao().recordQuery("gloomhaven_storyline").orderBy("created DESC").limit(1).one(latest);
