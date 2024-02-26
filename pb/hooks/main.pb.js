99beae2cc17288bed149e6416deff3b749aa7535
wikro <robert@wikro.org>
2024-02-27 00:09:19 +0100

    Test

--- pb/hooks/main.pb.js
+++ pb/hooks/main.pb.js
@@ -3,6 +3,6 @@ routerAdd("GET", "/gh", (c) => {
 
   $app.dao().recordQuery("gloomhaven_storyline").orderBy("created DESC").limit(1).one(latest);
 
-  return c.json(307, latest.url);
+  return c.json(307, "https://www.google.com");
 });
 
