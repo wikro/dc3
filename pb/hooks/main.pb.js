43caa929a73f01a66af996bbcaf0f7cad682f43b
wikro <robert@wikro.org>
2024-02-26 21:23:06 +0100

    Typo

--- pb/hooks/main.pb.js
+++ pb/hooks/main.pb.js
@@ -1,7 +1,7 @@
 routerAdd("GET", "/gh", (c) => {
   const latest = new Record();
 
-  $app.dao().recordQuery("gloomhaven_stroyline").orderBy("created DESC").limit(1).one(latest);
+  $app.dao().recordQuery("gloomhaven_storyline").orderBy("created DESC").limit(1).one(latest);
 
   return c.json(200, latest)
 });
