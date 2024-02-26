dde89ebb27348d8f59882e7987cce68265444f10
wikro <robert@wikro.org>
2024-02-27 00:51:25 +0100

    Trailing and non-trailing slash

--- pb/hooks/main.pb.js
+++ pb/hooks/main.pb.js
@@ -1,8 +1,10 @@
-routerAdd("GET", "/gh/", (c) => {
+const redirect = (c) => {
   const latest = new Record();
 
   $app.dao().recordQuery("gloomhaven_storyline").orderBy("created DESC").limit(1).one(latest);
 
   return c.redirect(307, latest.getString("url"));
-});
+};
 
+routerAdd("GET", "/gh", (c) => redirect(c));
+routerAdd("GET", "/gh/", (c) => redirect(c));
