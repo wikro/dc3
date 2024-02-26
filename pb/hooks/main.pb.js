7eee58d923a3d919203ae86ecafa2edccaa3a587
wikro <robert@wikro.org>
2024-02-27 00:59:18 +0100

    I guess no trailing slash

--- pb/hooks/main.pb.js
+++ pb/hooks/main.pb.js
@@ -1,10 +1,8 @@
-const redirect = (c) => {
+routerAdd("GET", "/gh", (c) => {
   const latest = new Record();
 
   $app.dao().recordQuery("gloomhaven_storyline").orderBy("created DESC").limit(1).one(latest);
 
   return c.redirect(307, latest.getString("url"));
-};
+});
 
-routerAdd("GET", "/gh", (c) => redirect(c));
-routerAdd("GET", "/gh/", (c) => redirect(c));
