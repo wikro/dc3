f96ce58658a99e9aabaf043917e6691c595d00eb
wikro <robert@wikro.org>
2024-02-26 21:05:23 +0100

    Fix

--- pb/hooks/main.pb.js
+++ pb/hooks/main.pb.js
@@ -1,8 +1,8 @@
 routerAdd("GET", "/gh", (c) => {
   const latest = new Record();
 
-  $app.dao().recordQuery("gloomhaven_stroyline").orderBy("created DESC").limit(1).one(record);
+  $app.dao().recordQuery("gloomhaven_stroyline").orderBy("created DESC").limit(1).one(latest);
 
-  return c.json(200, record)
+  return c.json(200, latest)
 });
 
