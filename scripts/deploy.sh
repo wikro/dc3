0c2fda8fdd0c82ea0192a0a8a8cbe2eb9065e97d
wikro <robert@wikro.org>
2024-10-04 14:37:43 +0200

    Cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,11 +12,3 @@ chmod 700 ~/.ssh
 chmod 600 ~/.ssh/known_hosts
 
 rsync --archive --delete html/ "$SSH":/var/www/html/
-
-#tar --transform 's/^./dist/' -cf /tmp/dist.tar .
-#
-#md5sum /tmp/dist.tar > /tmp/dist.tar.md5
-#
-#scp /tmp/dist.tar /tmp/dist.tar.md5 "$SSH":/tmp/
-#
-#ssh "$SSH" "md5sum -c /tmp/dist.tar.md5 && tar -xf /tmp/dist.tar -C /tmp && bash /tmp/dist/scripts/setup.sh"
