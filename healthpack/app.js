2019e72deee990ae7a5b87f246ac020f90fdaad9
wikro <robert@wikro.org>
2025-04-21 14:37:37 +0200

    Maybe in the right direction?

--- healthpack/app.js
+++ healthpack/app.js
@@ -1,10 +1,17 @@
 const data = {
+  core: [
+    { name: "Standing Shoulder Press", one_rep_max: 20 },
+    { name: "Deadlift", one_rep_max: 70 },
+  ],
+  supplement: [
+    { name: "Dip", sets: 5, reps: 15, weight: 20 },
+    { name: "Chin-up", sets: 5, reps: 10, weight: 50 },
+    { name: "Good Morning", sets: 5, reps: 12, weight: 30 },
+    { name: "Hanging Leg Raise", sets: 5, reps: 15, weight: "n/a" }
+  ],
   sessions: [
-    [
-      { name: "Standing Shoulder Press", sets: 1, reps: 5, weight: 20, done: false },
-      { name: "Dip", sets: 5, reps: 15, weight: 20, done: false },
-      { name: "Chin-up", sets: 5, reps: 10, weight: 50, done: false }
-    ]
+    { core: 0, supplements: [0, 1] },
+    { core: 1, supplements: [1, 2] }
   ]
 };
 
@@ -16,11 +23,62 @@ const model = {};
 
 // ===== Model
 
+model.core_sets = (core_id) => {
+  const core = data.core[core_id];
+  const core_sets = [];
+  const min_weight = 2.5;
+
+  for (let set = 1; set <= 3; set++) {
+    const core_set = {
+      done: false,
+      sets: 1,
+      reps: 5,
+      weight: Math.round((core.one_rep_max * (0.55 + set * 0.10)) / min_weight) * min_weight,
+      ...core
+    };
+
+    core_sets.push(core_set);
+  }
+
+  return core_sets;
+};
+
+model.supplement_sets = (supplement_ids) => {
+  const supplement_sets = [];
+
+  for (const supplement_id of supplement_ids) {
+    const supplement = data.supplement[supplement_id];
+
+    const supplement_set = {
+      done: false,
+      ...supplement
+    };
+
+    supplement_sets.push(supplement_set);
+  }
+
+  return supplement_sets;
+};
+
 model.init = () => {
+  const sessions = [];
+
+  for (const session_data of data.sessions) {
+    const session = [];
+
+    const core_sets = model.core_sets(session_data.core);
+    session.push(...core_sets);
+
+    const supplement_sets = model.supplement_sets(session_data.supplements);
+    session.push(...supplement_sets);
+
+    sessions.push(session);
+  }
+
   const proposal = {
     model: model,
     update: {
-      sessions: data.sessions
+      sessions: sessions
     }
   };
 
@@ -79,12 +137,12 @@ view.session = (session, session_id) => {
 };
 
 view.render = () => {
-  const app = document.getElementById("app");
+  const sessions = document.getElementById("sessions");
 
-  app.innerHTML = ``;
-  for (const i in model.sessions) {
-    const session = model.sessions[i];
-    app.innerHTML += view.session(session, i);
+  sessions.innerHTML = ``;
+  for (const session_id in model.sessions) {
+    const session = model.sessions[session_id];
+    sessions.innerHTML += view.session(session, session_id);
   }
 };
 
