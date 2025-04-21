cb291c329378d9267d030758bb4a3f83ee585366
wikro <robert@wikro.org>
2025-04-21 12:17:41 +0200

    Working sort of

--- healthpack/app.js
+++ healthpack/app.js
@@ -1,11 +1,11 @@
 const data = {
   sessions: [
     [
-      { name: "Standing Shoulder Press", sets: 1, reps: 5, weight: 20 },
-      { name: "Standing Shoulder Press", sets: 1, reps: 5, weight: 25 },
-      { name: "Standing Shoulder Press", sets: 1, reps: "5+", weight: 30 },
-      { name: "Dip", sets: 5, reps: 15, weight: 20 },
-      { name: "Chin-up", sets: 5, reps: 10, weight: 50 }
+      { name: "Standing Shoulder Press", sets: 1, reps: 5, weight: 20, done: false },
+      { name: "Standing Shoulder Press", sets: 1, reps: 5, weight: 25, done: false },
+      { name: "Standing Shoulder Press", sets: 1, reps: "5+", weight: 30, done: false },
+      { name: "Dip", sets: 5, reps: 15, weight: 20, done: false },
+      { name: "Chin-up", sets: 5, reps: 10, weight: 50, done: false }
     ]
   ]
 };
@@ -21,8 +21,8 @@ const model = {
 // ===== Model
 
 model.present = (proposal) => {
-  if (Object.hasOwn(proposal, "counter")) {
-    model.counter = proposal.counter;
+  if (proposal.exercise !== undefined) {
+    Object.assign(proposal.exercise, proposal.update);
   }
 
   state.update();
@@ -42,27 +42,25 @@ state.update = () => {
 
 // ===== Views
 
-view.exercise = (exercise) => {
-  return `
-    <li>${exercise.name}</li>
-    <li>${exercise.sets} x ${exercise.reps}</li>
-    <li>${exercise.weight} kg</li>
-  `;
-};
-
-view.session = (session) => {
+view.session = (session, session_id) => {
   let html = `
     <div class="session">
-      <h3>Session</h3>
-      <ul>
+      <h3>Session ${session_id}</h3>
   `;
 
-  for (const exercise of session) {
-    html += view.exercise(exercise);
+  for (const exercise_id in session) {
+    const exercise = session[exercise_id];
+    html +=  `
+      <ul ${exercise.done ? 'class="done"' : ''}>
+        <li>${exercise.name}</li>
+        <li>${exercise.sets} x ${exercise.reps}</li>
+        <li>${exercise.weight} kg</li>
+        <li><input type="checkbox" onclick="action.toggle_done(${session_id}, ${exercise_id});" ${exercise.done ? 'checked' : ''}/></li>
+      </ul>
+    `;
   }
 
   html += `
-      </ul>
     </div>
   `;
 
@@ -73,16 +71,24 @@ view.render = () => {
   const app = document.getElementById("app");
 
   app.innerHTML = ``;
-  for (const session of model.sessions) {
-    app.innerHTML += view.session(session);
+  for (const i in model.sessions) {
+    const session = model.sessions[i];
+    app.innerHTML += view.session(session, i);
   }
 };
 
 // ===== Actions
 
-action.inc_counter = () => {
-  const incremented = model.counter + 1;
-  model.present({ counter: incremented });
+action.toggle_done = (session_id, exercise_id) => {
+  const exercise = model.sessions[session_id][exercise_id];
+  const proposal = {
+    exercise: exercise,
+    update: {
+      done: !exercise.done
+    }
+  };
+
+  model.present(proposal);
 };
 
 // ===== Init
